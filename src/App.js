e7567f0ffbdb8baf25e7d4fbab21e5a5b41ffc49
kolbyjwilliams <kolbyjwilliams@gmail.com>
2020-12-01 15:27:34 -0600

    Added State Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+export default class App extends Component {
+//  Above we have created a class called App the extends the functionality of the Component class
+//  The export keyword above makes the class available for use outside of the JS file where it is created
+constructor(){
+  super();
+  //  React components have a special property called "state".  The "state" is used to define the state of data (props)
+this.state = {
+  todoOwner: "Kolby",
+  todoList: [
+    {action: "Move burn pile", done: false},
+    {action: "Oil Change", done: false},
+    {action: "Clean Room", done: false},
+    {action: "X-mas Shopping", done: false},
+    {action: "Do hella gaming", done: true}
+  ]
 }
+}// End of Constructor
+}// End of APP
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
