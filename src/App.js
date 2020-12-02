e71b5bbb53ce28394f3bca083d13130ddcd63ba7
kolbyjwilliams <kolbyjwilliams@gmail.com>
2020-12-02 10:49:30 -0600

    Added features 3 and 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import {ToDoBanner} from './TODOBANNER';
+import {ToDoRow} from './TODOROW';
 export default class App extends Component {
 //  Above we have created a class called App the extends the functionality of the Component class
 //  The export keyword above makes the class available for use outside of the JS file where it is created
@@ -19,6 +20,30 @@ this.state = {
   ]
 }
 }// End of Constructor
+
+//  Feature 3 & 4
+  //  If the ToDoRow Component's "done" property experiences a change event (ie. checking the Mark Complete box in the UI) then the ToDoRow Component calls a callback method called toggleToDo (below)  and passes toggleToDo the checked todo item
+  //  ----- Function to display table rows ------
+
+  todoTableRows = (finishedTask) => this.state.todoList.filter(
+    x  => x.done === finishedTask).map(y => 
+    <ToDoRow
+    key={y.action}
+    item={y}
+    callback={this.toggleToDo} // The callback will be invoked (executed, run) when everything in <ToDoRow> is finished AND the user clicks the input box
+    //  The data passed into the callback from the ToDoRow componet is passed automatically into the function defined in the callback
+    />
+  );
+
+//-----------------Function to toggle and item from done to not done or visversa
+toggleToDo = (checkedToDoItem) => this.setState(
+  {
+    todoList: this.state.todoList.map(
+       bob => bob.action === checkedToDoItem.action ? {...bob, done: !bob.done} : bob
+       )
+  }
+);
+
 render = () =>
 <div>
   {/*Features 1 and 2*/}
@@ -28,6 +53,18 @@ render = () =>
   todoList = {this.state.todoList}
 
 />
+
+{/* Freatures 3 and 4*/}
+<table className = "table table-striped table-bordered">
+  <thead>
+    <th>Action</th>
+    <th>Mark As Complete</th>
+  </thead>
+  <tbody>
+    {this.todoTableRows(false)}
+  </tbody>
+</table>
+
 </div>
 }// End of APP
 
---
+++ src/TODOROW.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export class ToDoRow extends Component{
+    // Feature 3 and 4
+
+    render = () =>
+    <tr>
+        <td>
+            {this.props.item.action}
+        </td>
+        <td>
+            <input 
+                type = "checkbox"
+                checked={this.props.item.done}
+                onChange={() => this.props.callback(this.props.item)}
+            />
+        </td>
+    </tr>
+}
